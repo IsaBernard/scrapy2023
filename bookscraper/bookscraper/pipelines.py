0357136de9928971d3e4a6ab81691a45543d4807
Isabelle Bernard <isabellebernard514@gmail.com>
2023-06-13 22:07:58 -0400

    started the pipeline by stripping whitespaces fron strings

--- bookscraper/bookscraper/pipelines.py
+++ bookscraper/bookscraper/pipelines.py
@@ -9,5 +9,15 @@ from itemadapter import ItemAdapter
 
 
 class BookscraperPipeline:
+
     def process_item(self, item, spider):
+        adapter = ItemAdapter(item)
+
+        # Strip all whitespaces from strings
+        field_names = adapter.field_names()
+        for field_name in field_names:
+            if field_name != 'description':
+                value = adapter.get(field_name)
+                adapter[field_name] = value.strip()
+
         return item
