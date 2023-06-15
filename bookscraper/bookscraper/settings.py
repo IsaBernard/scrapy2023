374efa82c8b5ad8130c502a4ac2c1477c95b819f
Isabelle Bernard <isabellebernard514@gmail.com>
2023-06-15 15:45:25 -0400

    updated settings.py to include FEEDS for saving to csv

--- bookscraper/bookscraper/settings.py
+++ bookscraper/bookscraper/settings.py
@@ -92,3 +92,9 @@ ITEM_PIPELINES = {
 REQUEST_FINGERPRINTER_IMPLEMENTATION = "2.7"
 TWISTED_REACTOR = "twisted.internet.asyncioreactor.AsyncioSelectorReactor"
 FEED_EXPORT_ENCODING = "utf-8"
+
+
+# configure the saving of file to csv
+FEEDS = {
+   'data/%(name)s/%(name)s_%(time)s.csv': {'format': 'csv', 'overwrite': True}
+}
